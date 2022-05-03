390f9bdf0a290c77ce069ba8eae66a4791c2c6fb
lomesh <badhelomesh82@gmail.com>
2022-05-03 16:03:42 +0530

    Feat: Loader Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import { Footer, Nav, Routelist } from 'frontend/component';
+import { Footer, Loader, Nav, Routelist } from 'frontend/component';
+import { useAppSelector } from 'frontend/utility';
 import React from 'react';
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import './App.css';
 
 function App() {
+
+  const { loader } = useAppSelector(state => state.user)
+
   return (
     <div className="App">
       <Nav />
       <Routelist />
       <Footer />   
+      {loader && <Loader />}
       <ToastContainer />
     </div>
   );
